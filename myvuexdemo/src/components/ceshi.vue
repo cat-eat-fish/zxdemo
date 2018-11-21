7425c55a8fb04c2da7e5fc6cca8546ebb9c76745
c_e_f <18434367522@example.com>
2018-11-21 15:36:35 +0800

    a

---
+++ myvuexdemo/src/components/ceshi.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ceshi',
+  data () {
+    return {
+     
+    }
+  },
+  created(){
+        fetch("/api/",{}).then(res=>{
+            return res.json()
+        }).then(data=>{
+            console.log(data)
+        })
+
+      this.$axios.get('/cityapi/region/init').then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        });
+  },
+  computed:{
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
